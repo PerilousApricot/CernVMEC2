e524fdba718f11332dba1f76c2102deeccdc0298
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2010-10-05 00:51:55 -0500

    fixing bootstrap

--- bootstrapEC2.sh
+++ bootstrapEC2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export HOME=/root/
 chmod +x *.sh
-echo 'cd /root' >> /etc/rc.local
+echo 'cd /root/Condor_glidein' >> /etc/rc.local
 echo './makePartitions.sh' >> /etc/rc.local 
 echo 'conary update squid' >> /etc/rc.local 
 echo 'rm /etc/squid/squid.conf' >> /etc/rc.local 
@@ -10,7 +10,7 @@ echo 'chown -R squid:squid squid' >> /etc/rc.local
 echo 'olddir=`pwd`' >> /etc/rc.local 
 echo 'cd /mnt/dumpspace/squid' >> /etc/rc.local 
 echo 'squid -z' >> /etc/rc.local 
-echo 'cd /root' >> /etc/rc.local 
+echo 'cd /root/Condor_glidein' >> /etc/rc.local 
 echo '/etc/init.d/squid stop' >> /etc/rc.local 
 echo '/etc/init.d/squid start' >> /etc/rc.local 
 echo './modifySiteLocalConfig.sh' >> /etc/rc.local 
