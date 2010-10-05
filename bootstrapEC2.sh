1245e6313180b5c3fef4c6a090d619505dab66bd
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2010-10-05 00:35:56 -0500

    fixing bootstrap

--- bootstrapEC2.sh
+++ bootstrapEC2.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 export HOME=/root/
 chmod +x *.sh
-./makePartitions.sh
-conary update squid
-rm /etc/squid/squid.conf
-cp squid.conf /etc/squid/squid.conf
-chown -R squid:squid squid
-olddir=`pwd`
-cd /mnt/dumpspace/squid
-squid -z
-cd $olddir
-/etc/init.d/squid stop
-/etc/init.d/squid start
-./modifySiteLocalConfig.sh
+echo 'cd /root' >> /etc/rc.local
+echo './makePartitions.sh' >> /etc/rc.local 
+echo 'conary update squid' >> /etc/rc.local 
+echo 'rm /etc/squid/squid.conf' >> /etc/rc.local 
+echo 'cp squid.conf /etc/squid/squid.conf' >> /etc/rc.local 
+echo 'chown -R squid:squid squid' >> /etc/rc.local 
+echo 'olddir=`pwd`' >> /etc/rc.local 
+echo 'cd /mnt/dumpspace/squid' >> /etc/rc.local 
+echo 'squid -z' >> /etc/rc.local 
+echo 'cd /root' >> /etc/rc.local 
+echo '/etc/init.d/squid stop' >> /etc/rc.local 
+echo '/etc/init.d/squid start' >> /etc/rc.local 
+echo './modifySiteLocalConfig.sh' >> /etc/rc.local 
 env | awk '/.*/ {print "export " $0}' >> /etc/rc.local
+
+
+
+
 echo '#none' >> /etc/rc.local
 echo './startcondor-amazon.sh' >> /etc/rc.local
--- makePartitions.sh
+++ makePartitions.sh
@@ -3,7 +3,7 @@ fdisk /dev/sdb < fdiskscript.stdin
 fdisk /dev/sdc < fdiskscript.stdin
 pvcreate /dev/sdb2 /dev/sdc2
 vgcreate dumpspace /dev/sdb2 /dev/sdc2
-lvcreate -i 2 -ndumpvolume -L830G dumpspace
+lvcreate -i 2 -ndumpvolume -L400G dumpspace
 mkfs.ext3 /dev/dumpspace/dumpvolume
 mkdir /mnt/dumpspace
 mount -t ext3 /dev/dumpspace/dumpvolume /mnt/dumpspace
